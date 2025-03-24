8822c9dacd480bf02b7bb7a79eb82840b0120d53
ivanbezniak <ivan.bezniak@gmail.com>
2025-03-24 16:20:45 +0300

    9 commit SEO

--- src/common/MetaTags.jsx
+++ src/common/MetaTags.jsx
@@ -1,32 +1,32 @@
-// import React, { useEffect } from "react";
-// import { Helmet } from "react-helmet-async";
-// import PropTypes from "prop-types";
-//
-// const MetaTags = ({ title, description, keywords }) => {
-//     useEffect(() => {
-//         document.title = title; // Мгновенно обновляем заголовок страницы
-//     }, [title]); // Следим за изменением title
-//
-//     return (
-//         <Helmet>
-//             <meta name="description" content={description} />
-//             {keywords && <meta name="keywords" content={keywords} />}
-//         </Helmet>
-//     );
-// };
-//
-// // Проверка типов пропсов
-// MetaTags.propTypes = {
-//     title: PropTypes.string.isRequired,
-//     description: PropTypes.string.isRequired,
-//     keywords: PropTypes.string,
-// };
-//
-// // Значения по умолчанию
-// MetaTags.defaultProps = {
-//     title: "Заголовок страницы",
-//     description: "Описание страницы по умолчанию",
-//     keywords: "",
-// };
-//
-// export default MetaTags;
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet-async";
+import PropTypes from "prop-types";
+
+const MetaTags = ({ title, description, keywords }) => {
+    useEffect(() => {
+        document.title = title; // Мгновенно обновляем заголовок страницы
+    }, [title]); // Следим за изменением title
+
+    return (
+        <Helmet>
+            <meta name="description" content={description} />
+            {keywords && <meta name="keywords" content={keywords} />}
+        </Helmet>
+    );
+};
+
+// Проверка типов пропсов
+MetaTags.propTypes = {
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    keywords: PropTypes.string,
+};
+
+// Значения по умолчанию
+MetaTags.defaultProps = {
+    title: "Заголовок страницы",
+    description: "Описание страницы по умолчанию",
+    keywords: "",
+};
+
+export default MetaTags;
